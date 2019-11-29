d3f4956a32eade0211303fd50f9cd64ac8a41d6c
manuelwecker <manuelwecker@web.de>
2019-11-29 15:31:44 +0100

    Add basic page structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
 import Header from "./components/Header";
-import Main from "./components/Main";
 import GlobalStyles from "./GlobalStyles";
+import styled from "@emotion/styled";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AddFood from "./pages/AddFood";
+import FoodList from "./pages/FoodList";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-flow: column wrap;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Header></Header>
-      <Main></Main>
+      <Router>
+        <Container>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <FoodList />
+            </Route>
+            <Route path="/add">
+              <AddFood />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
     </>
   );
 }
