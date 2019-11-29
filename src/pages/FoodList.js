b4aee7a650137389a7387ccf66963a92006e8739
manuelwecker <manuelwecker@web.de>
2019-11-29 16:57:24 +0100

    Map array from fakeapi

--- src/pages/FoodList.js
+++ src/pages/FoodList.js
@@ -3,28 +3,37 @@ import styled from "@emotion/styled";
 
 const Card = styled.div`
   background: hotpink;
-  height: 250px;
+  height: 120px;
   width: 80vw;
   margin: auto;
   display: flex;
   flex-direction: column;
+  border: 1px solid black;
 `;
 
 export default function FoodList() {
+  const [foodItems, setFoodItems] = React.useState([]);
+
   async function getFoodItems() {
     const response = await fetch("http://localhost:5678/items");
     const newItems = await response.json();
-    console.log(newItems);
+    setFoodItems(newItems);
   }
 
-  getFoodItems();
+  React.useEffect(() => {
+    getFoodItems();
+  }, []);
 
   return (
-    <Card>
-      <div>Cat</div>
-      <div>What</div>
-      <div>Date</div>
-      <div>kcal</div>
-    </Card>
+    <div>
+      {foodItems.map(item => (
+        <Card>
+          <div>{item.cat}</div>
+          <div>{item.what}</div>
+          <div>{item.date}</div>
+          <div>{item.kcal}</div>
+        </Card>
+      ))}
+    </div>
   );
 }
