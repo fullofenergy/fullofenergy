a3706332b805e65b44469f679d84f8cc70f1fe33
manuelwecker <manuelwecker@web.de>
2019-11-29 16:33:54 +0100

    Add fetch to fake api

--- src/pages/FoodList.js
+++ src/pages/FoodList.js
@@ -1,5 +1,30 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Card = styled.div`
+  background: hotpink;
+  height: 250px;
+  width: 80vw;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`;
 
 export default function FoodList() {
-  return <div>List all Food items here</div>;
+  async function getFoodItems() {
+    const response = await fetch("http://localhost:5678/items");
+    const newItems = await response.json();
+    console.log(newItems);
+  }
+
+  getFoodItems();
+
+  return (
+    <Card>
+      <div>Cat</div>
+      <div>What</div>
+      <div>Date</div>
+      <div>kcal</div>
+    </Card>
+  );
 }
