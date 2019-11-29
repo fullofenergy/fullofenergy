ae5f9a83b909b9b5c0defc795f83efc5a6aa88be
manuelwecker <manuelwecker@web.de>
2019-11-29 15:35:35 +0100

    Remove unused import

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Global, css } from "@emotion/core";
 import themes from "./utils/themes";
-import { useTheme } from "emotion-theming";
 
 export default function GlobalStyles() {
-  const theme = useTheme();
   return (
     <Global
       styles={css`
